f6edabdee0270fd474c98e18f20f3c047b7ff658
aimable ukobizaba <aimableukobizaba@gmail.com>
2025-02-14 06:06:04 +0200

    Added userprofile

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
+import UserProfile from './components/UserProfile'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
@@ -15,6 +16,7 @@ function App() {
       <div>
       <Header />
       <MainContent />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <Footer />
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
