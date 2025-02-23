cdc0c1842d0915afa15a6dcf0db159e71bcca41b
aimable ukobizaba <aimableukobizaba@gmail.com>
2025-02-23 11:08:46 +0200

    added my-company folder

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Home from "./components/Home";
-import About from "./components/About";
+import Navbar from "./Components/Navbar";
+import Home from "./Components/Home";
+import About from "./Components/About";
 import Services from "./Components/Services";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
+import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
