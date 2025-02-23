906edacdddc3038fb87939e8d2f7bfdb7e64adbb
aimable ukobizaba <aimableukobizaba@gmail.com>
2025-02-23 11:01:31 +0200

    added my-company folder

---
+++ my-company/src/Components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav style={{ background: "#333", padding: "10px", textAlign: "center" }}>
+      <Link to="/" style={{ color: "white", margin: "10px" }}>Home</Link>
+      <Link to="/about" style={{ color: "white", margin: "10px" }}>About</Link>
+      <Link to="/services" style={{ color: "white", margin: "10px" }}>Services</Link>
+      <Link to="/contact" style={{ color: "white", margin: "10px" }}>Contact</Link>
+    </nav>
+  );
+};
+
+export default Navbar;
