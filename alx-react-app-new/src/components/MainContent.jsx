ad2920c2b280252beab919abc7d28c338d62707e
aimable ukobizaba <aimableukobizaba@gmail.com>
2025-02-23 09:33:43 +0200

    added the alx-react-app-new folder

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,8 @@
+function MainContent() {
+    return (
+        <main>
+            <p style={{backgroundColor:"red", padding:"10px"}}>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    )
+}
+export default MainContent;
