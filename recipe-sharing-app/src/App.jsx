7e7147e88b0d70ad08211242815e9d99cb4ca288
aimable ukobizaba <aimableukobizaba@gmail.com>
2025-03-01 21:41:06 +0200

    added some functionality

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,16 +1,23 @@
 import { useState } from 'react'
 import RecipeList from './components/RecipeList'
 import AddRecipeForm from './components/AddRecipeForm'
+import RecipeDetails from './components/RecipeDetails'
+import DeleteRecipeButton from './components/DeleteRecipeButton'
+import EditRecipeForm from './components/EditRecipeForm'
+
 import './App.css'
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <RecipeList />
-      <AddRecipeForm />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<RecipeList />} />
+        <Route path="/add" element={<AddRecipeForm />} />
+        <Route path="/recipe/:id" element={<RecipeDetails />} />
+      </Routes>
+    </Router>
   )
 }
 
