6f5e86f6bae2f91f02b6f7e241d734a53f7985ee
UKOBIZABA Aimable <154277022+AimableTrague@users.noreply.github.com>
2025-03-09 15:51:11 +0200

    Added react todo app

---
+++ react-todo/src/components/TodoList.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import AddTodoForm from "./AddTodoForm";
+
+const initialTodos = [
+  { id: 1, text: "Learn React", completed: false },
+  { id: 2, text: "Build a Todo App", completed: true },
+];
+
+function TodoList() {
+  const [todos, setTodos] = useState(initialTodos);
+
+  const addTodo = (text) => {
+    setTodos([...todos, { id: Date.now(), text, completed: false }]);
+  };
+
+  const toggleTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  return (
+    <div>
+      <h2>Todo List</h2>
+      <AddTodoForm addTodo={addTodo} />
+      <ul>
+        {todos.map((todo) => (
+          <li
+            key={todo.id}
+            style={{ textDecoration: todo.completed ? "line-through" : "none" }}
+            onClick={() => toggleTodo(todo.id)}
+          >
+            {todo.text}
+            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default TodoList;
