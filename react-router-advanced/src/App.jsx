fec9bf106e849a81d28f9154b41b3de96068bbec
UKOBIZABA Aimable <154277022+AimableTrague@users.noreply.github.com>
2025-03-09 15:06:37 +0200

    added simple function

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -4,24 +4,19 @@ import Profile from './components/Profile';
 import ProfileDetails from './components/ProfileDetails';
 import ProfileSettings from './components/ProfileSettings';
 import Home from './components/Home';
+import BlogPost from './components/BlogPost'; // Import the BlogPost component
 
 function App() {
   return (
     <Router>
       <nav>
         <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/profile">Profile</Link>
-          </li>
-          <li>
-            <Link to="/details/123">Profile Details</Link>
-          </li>
-          <li>
-            <Link to="/settings/123">Profile Settings</Link>
-          </li>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/profile">Profile</Link></li>
+          <li><Link to="/details/123">Profile Details</Link></li>
+          <li><Link to="/settings/123">Profile Settings</Link></li>
+          <li><Link to="/blog/1">Blog Post 1</Link></li>
+          <li><Link to="/blog/2">Blog Post 2</Link></li>
         </ul>
       </nav>
 
@@ -30,6 +25,7 @@ function App() {
         <Route path="/profile" element={<Profile />} />
         <Route path="/details/:userId" element={<ProfileDetails />} />
         <Route path="/settings/:userId" element={<ProfileSettings />} />
+        <Route path="/blog/:id" element={<BlogPost />} /> {/* Dynamic Route */}
       </Routes>
     </Router>
   );
