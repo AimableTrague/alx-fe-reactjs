493ecbca71ad93a783220ddee446000c187167d6
UKOBIZABA Aimable <154277022+AimableTrague@users.noreply.github.com>
2025-03-09 12:59:02 +0200

    Adde my directory of useQuery

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -1,33 +1,28 @@
 import React from 'react'
-import { useQuery } from 'react-query'
+import { useQuery } from '@tanstack/react-query'
 
 const fetchData = async () => {
-    const res = await fetch("https://jsonplaceholder.typicode.com/posts")
-    return res.json()
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts")
+  return res.json()
 }
 
 const PostsComponent = () => {
-    const { data, error, isLoading, refetch } = useQuery('fetchData', fetchData, {
-        staleTime: 5 * 60 * 1000,
-    })
+  const { data, error, isLoading, isError, refetch } = useQuery('fetchData', fetchData)
 
-    if (isLoading) return <div>Loading...</div>
-    if (error) return <div>Error fetching Posts</div>
+  if (isLoading) return <div>Loading...</div>
 
-    return (
-        <div>
-            <h1>Posts</h1>
-            <button onClick={() => refetch()}>Refetch Data</button>
-            <div>
-                {data.map((item) => (
-                    <div key={item.id}>
-                        <h3>{item.title}</h3>
-                        <p>{item.body}</p>
-                    </div>
-                ))}
-            </div>
-        </div>
-    )
+  if (isError) return <div>Error fetching Posts</div>
+
+  return (
+    <div>
+      {data.map((item) => {
+        return <div key={item.userId}>{item.title}</div>
+      })}
+
+      {/* Button to manually trigger a refetch */}
+      <button onClick={() => refetch()}>Refetch Posts</button>
+    </div>
+  )
 }
 
 export default PostsComponent;
