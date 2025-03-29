fe0cc472b1bd6e7e326ef5af5095ad7aaf410510
UKOBIZABA Aimable <154277022+AimableTrague@users.noreply.github.com>
2025-03-29 18:03:54 +0200

    my first commit ofr this github app

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,16 @@
+import API from './components/services/API'
+import './index.css'
+
+
+
+
+function App() {
+
+  return (
+    <>  
+      <API />
+    </>
+  )
+}
+
+export default App
---
+++ github-user-search/src/components/services/API.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const API = () => {
+    const API_KEY = import.meta.env.VITE_APP_GITHUB_API_KEY
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default API
